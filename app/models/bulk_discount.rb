5fda01081ef8d19687590a929ba1a3172ff5469c
Alex Schwartz <aschwartz@detaso.com>
2021-03-04 20:44:06 -0600

    Add name to bulk_discounts

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,11 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
+  validates_presence_of :name
   validates :threshold, presence: true, numericality: { only_integer: true }
   validates :percent_discount, presence: true, numericality: { only_integer: true }
+
+  def self.for(merchant_id)
+    where(merchant_id: merchant_id)
+  end
 end
