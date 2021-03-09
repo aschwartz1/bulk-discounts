bab7fc943990181381fc378e65b862ccef217d7c
Alex Schwartz <aschwartz@detaso.com>
2021-03-08 22:58:23 -0600

    Fix errors from method rename

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -14,44 +14,17 @@ RSpec.describe BulkDiscount, type: :model do
   end
 
   describe 'class methods' do
-    describe '::for' do
+    describe '::for_merchant' do
       it 'returns bulk discounts for merchant' do
         merchant = create(:merchant)
         expected = create_list(:bulk_discount, 3, merchant: merchant)
 
-        expect(BulkDiscount.for(merchant.id)).to eq(expected)
+        expect(BulkDiscount.for_merchant(merchant.id)).to eq(expected)
       end
 
       it 'returns an empty array if no discounts exist for merchant' do
         merchant = create(:merchant)
-        expect(BulkDiscount.for(merchant.id)).to eq([])
-      end
-    end
-
-    describe '::by_merchant_order_discount_threshold_desc' do
-      it "returns correctly sorted list of a merchant's discounts" do
-        merchant = create(:merchant)
-        discount_1 = create(:bulk_discount, threshold: 8, percent_discount: 30, merchant_id: merchant.id)
-        discount_2 = create(:bulk_discount, threshold: 6, percent_discount: 20, merchant_id: merchant.id)
-        discount_3 = create(:bulk_discount, threshold: 4, percent_discount: 10, merchant_id: merchant.id)
-
-        expected_order = [discount_1, discount_2, discount_3]
-        expect(BulkDiscount.by_merchant_order_discount_threshold_desc(merchant.id)).to eq(expected_order)
-      end
-
-      it 'returns correct order when unreachable discount percent exists' do
-        merchant = create(:merchant)
-        discount_1 = create(:bulk_discount, threshold: 8, percent_discount: 30, merchant_id: merchant.id)
-        discount_2 = create(:bulk_discount, threshold: 6, percent_discount: 10, merchant_id: merchant.id)
-        discount_3 = create(:bulk_discount, threshold: 4, percent_discount: 20, merchant_id: merchant.id)
-
-        expected_order = [discount_1, discount_3, discount_2]
-        expect(BulkDiscount.by_merchant_order_discount_threshold_desc(merchant.id)).to eq(expected_order)
-      end
-
-      it 'returns empty array when no discounts exist' do
-        merchant = create(:merchant)
-        expect(BulkDiscount.by_merchant_order_discount_threshold_desc(merchant.id)).to eq([])
+        expect(BulkDiscount.for_merchant(merchant.id)).to eq([])
       end
     end
 
